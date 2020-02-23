b96690f34e07add1d0cd6505ab63533f2bda92a9
joaerazogo <joaerazogo@unal.edu.co>
2020-02-23 18:04:12 -0500

    Begin of the project

---
+++ backend/expenses/src/main/java/com/example/expenses/ExpensesApplication.java
@@ -0,0 +1,14 @@
+package com.example.expenses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensesApplication {
+
+	public static void main(String[] args) {
+		//this is the begin of the application
+		SpringApplication.run(ExpensesApplication.class, args);
+	}
+
+}
